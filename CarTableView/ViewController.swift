bd4548046764dc6dfe6faec24b7d8274a18888b2
Valeriy Kovalevskiy <kovalevskiy2021@gmail.com>
2020-02-13 17:18:53 +0300

    Initial Commit

---
+++ CarTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarTableView
+//
+//  Created by Valeriy Kovalevskiy on 2/13/20.
+//  Copyright © 2020 v.kovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
